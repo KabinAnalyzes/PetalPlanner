b5e7c739189c08298feee3913e15321b25da8910
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-06-05 02:40:47 -0700

    added caching for motivational quotes

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,15 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, Blueprint
 from flask_login import login_required, current_user
+from sqlalchemy.exc import IntegrityError
 import requests
+from . import db
+from .models import Quote
+from datetime import date
+from . import cache
 
 main= Blueprint('main', __name__)
 
+
 @main.route('/')
+@cache.cached(timeout=86400)
 def index():
-    # url = "https://zenquotes.io/api/today"
-    # response = requests.get(url).json()
-    # quote = response[0]['q'] + "\r\n - " + response[0]['a']
-    return render_template('index.html')
+    url = "https://zenquotes.io/api/today"
+    response = requests.get(url).json()
+    author = str(response[0]['a'])
+    quote = str(response[0]['q'])
+    text = quote + "\r\n - " + author
+    return render_template('index.html', quote=text)
+
 
 @main.route('/profile')
 @login_required
