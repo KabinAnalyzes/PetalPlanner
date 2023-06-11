ed84c62e4ae5c348ed2f2cbe9f1db95fa203430a
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-06-11 03:01:12 -0700

    Added basic ToDo List table & functionality

--- flaskr/main.py
+++ flaskr/main.py
@@ -5,20 +5,49 @@ import requests
 from . import db
 from datetime import date
 from . import cache
+from .models import Todo
 
 main= Blueprint('main', __name__)
 
-
 @main.route('/')
-@cache.cached(timeout=86400)
+def home():
+    return render_template('home.html')
+
+@main.get('/todo')
+#@cache.cached(timeout=86400)
+@login_required
 def index():
     url = "https://zenquotes.io/api/today"
     response = requests.get(url).json()
     author = str(response[0]['a'])
     quote = str(response[0]['q'])
     text = quote + "\r\n - " + author
-    return render_template('index.html', quote=text)
+    todo_list = db.session.query(Todo).all()
+    return render_template('index.html', quote=text, todo_list=todo_list)
 
+@main.post('/add')
+@login_required
+def add():
+    title = request.form.get("title")
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+@main.get("/update/<int:todo_id>")
+@login_required
+def update(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("main.index"))
+
+@main.get("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = db.session.query(Todo).filter(Todo.id == todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("main.index"))
 
 @main.route('/profile')
 @login_required
--- flaskr/models.py
+++ flaskr/models.py
@@ -14,7 +14,7 @@ class User(UserMixin, db.Model):
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
+    title = db.Column(db.String(200), nullable=False)
     complete = db.Column(db.Boolean)
 
     def __repr__(self):
