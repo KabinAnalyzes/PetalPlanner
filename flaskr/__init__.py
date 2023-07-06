97e9359894300256bc9fea51e5811a8f10ee97dd
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-07-06 03:58:51 -0700

    fix

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -23,7 +23,7 @@ def create_app():
 
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+    login_manager.init_app(application)
 
     from .models import User
 
