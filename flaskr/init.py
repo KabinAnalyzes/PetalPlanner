33cc6e83024870dfbb9b3eca2c08f686da8d947e
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-07-06 02:32:43 -0700

    fix

--- flaskr/init.py
+++ flaskr/init.py
@@ -9,37 +9,28 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 db = SQLAlchemy()
 cache = Cache(config={'CACHE_TYPE': 'simple','CACHE_DEFAULT_TIMEOUT': 86400})
 
-def create_app():
 
-    app=Flask(__name__)
-    cache.init_app(app)
+app=Flask(__name__)
+cache.init_app(app)
+app.config['SECRET_KEY'] = "secret-key"
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+'sqlite:///' + os.path.join(basedir, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
+login_manager.init_app(app)
+from .models import User
+@login_manager.user_loader
+def load_user(user_id):
+    # since the user_id is just the primary key of our user table, use it in the query for the user
+    return User.query.get(int(user_id))
 
-    app.config['SECRET_KEY'] = "secret-key"
-    app.config['SQLALCHEMY_DATABASE_URI'] =\
-    'sqlite:///' + os.path.join(basedir, 'data.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+from .main import main as main_blueprint
+app.register_blueprint(main_blueprint)
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
 
-    db.init_app(app)
-
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
-
-    from .models import User
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
-        return User.query.get(int(user_id))
-    
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
-
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
-
-    return app
 
 if __name__ == "__main__":
-    app = create_app()
     app.run(debug=True)
