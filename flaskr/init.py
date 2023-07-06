af6d21bacc1261c738bc1bd6b6af9200c07b5587
Kevin Loun <86264301+KabinAnalyzes@users.noreply.github.com>
2023-07-06 02:49:13 -0700

    fix?

---
+++ flaskr/init.py
@@ -0,0 +1,47 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+import os
+from flask_caching import Cache
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+db = SQLAlchemy()
+cache = Cache(config={'CACHE_TYPE': 'simple','CACHE_DEFAULT_TIMEOUT': 86400})
+
+def create_app():
+
+    app=Flask(__name__)
+    cache.init_app(app)
+
+    app.config['SECRET_KEY'] = "secret-key"
+    app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    from .models import User
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        # since the user_id is just the primary key of our user table, use it in the query for the user
+        return User.query.get(int(user_id))
+    
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    return app
+
+app = create_app()
+app.run()
+# if __name__ == "__main__":
+#     app = create_app()
+#     app.run()
